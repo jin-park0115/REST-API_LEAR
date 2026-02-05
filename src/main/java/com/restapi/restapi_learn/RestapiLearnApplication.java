eba7e111ac0922c9a3051503ba9819d319cb7ccf
jin-park0115 <jinpark0115@naver.com>
2026-02-05 17:08:59 +0900

    first commit

---
+++ src/main/java/com/restapi/restapi_learn/RestapiLearnApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.restapi_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiLearnApplication.class, args);
+    }
+
+}
