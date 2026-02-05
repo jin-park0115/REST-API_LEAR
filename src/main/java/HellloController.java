00ef552884e83b4e14bbbd35268128d7c947c1c3
jin-park0115 <jinpark0115@naver.com>
2026-02-05 17:12:39 +0900

    RestController

---
+++ src/main/java/HellloController.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HellloController {
+    @GetMapping("/hello")
+    public String hell() {
+        return "hello spring";
+    }
+}
+
+// RestContoller -> "이 클래스는 HTTP 요청을 받아서 응답하는 애다"
+// GetMapping -> "GET /hello 요청이 오면 이 메서드를 실행해라"
